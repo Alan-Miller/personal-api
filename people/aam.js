2e9a731811bc0a2651438f2857a244230b8c3974
Alan-Miller <alan.ashman@gmail.com>
2016-09-26 22:11:52 -0600

    try2

---
+++ people/aam.js
@@ -0,0 +1,60 @@
+module.exports = {
+  "name": "Alan",
+  "location": "Provo",
+  "occupations": ["cage builder", "cake maker", "courier", "clerk"],
+  "hobbies": [
+    {
+      "hobby": "drawing",
+      "type": "creative"
+    },
+    {
+      "hobby": "reading",
+      "type": "relaxing"
+    },
+    {
+      "hobby": "writing",
+      "type": "challenging"
+    },
+    {
+      "hobby": "making things",
+      "type": "creative"
+    }
+  ],
+  "skills":
+  [
+    {
+      "id": 1,
+      "name": "drawing",
+      "experience": "advanced"
+    },
+    {
+      "id": 2,
+      "name": "writing",
+      "experience": "intermediate"
+    },
+    {
+      "id": 3,
+      "name": "yarn animals",
+      "experience": "intermediate"
+    },
+    {
+      "id": 4,
+      "name": "cooking",
+      "experience": "pre-beginner"
+    },
+    {
+      "id": 5,
+      "name": "JavaScript",
+      "experience": "intermediate"
+    }
+  ],
+  "secrets":
+  [
+    {
+      "secret": "Stars",
+    },
+    {
+      "secret": "Fuzzy",
+    }
+  ]
+};
